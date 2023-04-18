c52e353f790a0d5545b0195ec1e6309cdc7ee22c
Gergely Meszaros <gergely@streamhpc.com>
2023-04-18 13:55:42 +0000

    Pre-process fortran sources before running doxygen
    Doxygen doesn't seem to properly parse fortran sources with pre-processing
    even with upper-case filenames.
    As a workaround run pre-processing with gfortran to a temp directory
    and set the source to that in conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,44 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import glob
 import os
+import shutil
+import subprocess
+from pathlib import Path
+
 from rocm_docs import ROCmDocs
+
+# Preprocess the fortran sources with gfortran, because doxygen doesn't seem
+# to correctly parse them, even with preprocessing turned on (and with
+# upper-case file names as convention)
+# preprocessed_out_dir has to be kept in sync with the Doxyfile's INPUT
+preprocessed_out_dir = Path("doxygen", "input")
+try:
+    os.mkdir(preprocessed_out_dir)
+except FileExistsError:
+    pass
+
+gfortran_exe = shutil.which("gfortran")
+if gfortran_exe is None:
+    raise RuntimeError("Couldn't find the fortran compiler!")
+
+for filename in glob.glob("../lib/hipfort/*.f"):
+    path = Path(filename)
+    # -P is to disable embedding line information
+    subprocess.check_call(
+        [
+            gfortran_exe,
+            "-E",
+            "-cpp",
+            "-P",
+            "-DUSE_FPOINTER_INTERFACES=1",
+            "-UUSE_CUDA_NAMES",
+            str(path),
+            "-o",
+            str(preprocessed_out_dir / path.name),
+        ]
+    )
 
 docs_core = ROCmDocs("hipfort Documentation")
 docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
