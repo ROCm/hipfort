7c51b037ca7f0f51d36fa19a6374a053dfcfb2ed
Sam Wu <sam.wu2@amd.com>
2023-07-07 09:49:17 -0600

    set pdf title, author, copyright, and version

--- docs/conf.py
+++ docs/conf.py
@@ -49,8 +49,15 @@ with open('../CMakeLists.txt', encoding='utf-8') as f:
     match = re.search(r'.*\bHIPFORT_VERSION\s+\"?([0-9.]+)[^0-9.]+', f.read())
     if not match:
         raise ValueError("HIPFORT_VERSION not found!")
-    version = match[1]
-left_nav_title = f"hipfort {version} Documentation"
+    version_number = match[1]
+left_nav_title = f"hipfort {version_number} Documentation"
+
+# for PDF output on Read the Docs
+project = "ROCm Documentation"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved."
+version = version_number
+release = version_number
 
 docs_core = ROCmDocs(left_nav_title)
 docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
