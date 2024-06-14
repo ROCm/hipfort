ROCm/hipfort

commit b82a8d14190a7fc6affae87b7d37d3fa071e6a94

Match subroutine names to C macro names
The ABI names are roctxMarkA, roctxRangePushA and roctxRangeStartA, but
in C these are exposed to users via macros that drop the A suffix. To
match the C interface as perceived by users, we can match the Fortran
subroutine name to the macro name rather than the actual symbol name.
Thank you to Brian Cornille and Dominic Charrier for this suggestion.

lib/hipfort/hipfort_roctx.f
@@ -27,13 +27,13 @@
 module hipfort_roctx
 
    interface
-      subroutine roctxMarkA(message) bind(c, name="roctxMarkA")
+      subroutine roctxMark(message) bind(c, name="roctxMarkA")
          use iso_c_binding, only: c_char
          implicit none
          character(kind=c_char) :: message(*)
       end subroutine roctxMarkA
 
-      function roctxRangePushA(message) bind(c, name="roctxRangePushA")
+      function roctxRangePush(message) bind(c, name="roctxRangePushA")
          use iso_c_binding, only: c_int,&
                                   c_char
          implicit none
@@ -47,7 +47,7 @@ module hipfort_roctx
          integer(c_int) :: roctxRangePop
       end function roctxRangePop
 
-      function roctxRangeStartA(message) bind(c, name="roctxRangeStartA")
+      function roctxRangeStart(message) bind(c, name="roctxRangeStartA")
          use iso_c_binding, only: c_size_t,&
                                   c_char
          implicit none

commit d21f26aef90a1261389a76a9456d79048b423c52

Fix roctx function names
These errors were introduced in b82a8d14190a7fc6affae87b7d37d3fa0 when
the functions were renamed without the trailing A.

lib/hipfort/hipfort_roctx.f
@@ -31,15 +31,15 @@ module hipfort_roctx
          use iso_c_binding, only: c_char
          implicit none
          character(kind=c_char) :: message(*)
-      end subroutine roctxMarkA
+      end subroutine roctxMark
 
       function roctxRangePush(message) bind(c, name="roctxRangePushA")
          use iso_c_binding, only: c_int,&
                                   c_char
          implicit none
-         integer(c_int) :: roctxRangePushA
+         integer(c_int) :: roctxRangePush
          character(kind=c_char) :: message(*)
-      end function roctxRangePushA
+      end function roctxRangePush
 
       function roctxRangePop() bind(c, name="roctxRangePop")
          use iso_c_binding, only: c_int
@@ -51,9 +51,9 @@ module hipfort_roctx
          use iso_c_binding, only: c_size_t,&
                                   c_char
          implicit none
-         integer(c_size_t) :: roctxRangeStartA
+         integer(c_size_t) :: roctxRangeStart
          character(kind=c_char) :: message(*)
-      end function roctxRangeStartA
+      end function roctxRangeStart
 
       subroutine roctxRangeStop(range_id) bind (c, name="roctxRangeStop")
          use iso_c_binding, only: c_size_t