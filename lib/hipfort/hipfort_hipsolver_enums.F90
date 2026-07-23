8a0d1f32de3b581512f0ff7941c01a87d6e2bd5c
Alexis Montoison <Alexis.Montoison@amd.com>
2026-07-22 22:23:32 -0600

    Regenerate hipSOLVER Fortran bindings (existing API)

--- lib/hipfort/hipfort_hipsolver_enums.F90
+++ lib/hipfort/hipfort_hipsolver_enums.F90
@@ -2,19 +2,19 @@
 ! ==============================================================================
 ! hipfort: FORTRAN Interfaces for GPU kernels
 ! ==============================================================================
-! Copyright (c) 2020-2022 Advanced Micro Devices, Inc. All rights reserved.
+! Copyright (c) 2020-2026 Advanced Micro Devices, Inc. All rights reserved.
 ! [MITx11 License]
-! 
+!
 ! Permission is hereby granted, free of charge, to any person obtaining a copy
 ! of this software and associated documentation files (the "Software"), to deal
 ! in the Software without restriction, including without limitation the rights
 ! to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 ! copies of the Software, and to permit persons to whom the Software is
 ! furnished to do so, subject to the following conditions:
-! 
+!
 ! The above copyright notice and this permission notice shall be included in
 ! all copies or substantial portions of the Software.
-! 
+!
 ! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -23,11 +23,12 @@
 ! OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 ! THE SOFTWARE.
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-          
-           
+
 module hipfort_hipsolver_enums
+  use, intrinsic :: iso_c_binding
   implicit none
 
+  ! hipsolverStatus_t
   enum, bind(c)
     enumerator :: HIPSOLVER_STATUS_SUCCESS = 0
     enumerator :: HIPSOLVER_STATUS_NOT_INITIALIZED = 1
@@ -41,83 +42,107 @@ module hipfort_hipsolver_enums
     enumerator :: HIPSOLVER_STATUS_HANDLE_IS_NULLPTR = 9
     enumerator :: HIPSOLVER_STATUS_INVALID_ENUM = 10
     enumerator :: HIPSOLVER_STATUS_UNKNOWN = 11
+    enumerator :: HIPSOLVER_STATUS_ZERO_PIVOT = 12
+    enumerator :: HIPSOLVER_STATUS_MATRIX_TYPE_NOT_SUPPORTED = 13
   end enum
 
+  ! hipblasOperation_t
   enum, bind(c)
     enumerator :: HIPSOLVER_OP_N = 111
     enumerator :: HIPSOLVER_OP_T = 112
     enumerator :: HIPSOLVER_OP_C = 113
   end enum
 
+  ! hipblasFillMode_t
   enum, bind(c)
     enumerator :: HIPSOLVER_FILL_MODE_UPPER = 121
     enumerator :: HIPSOLVER_FILL_MODE_LOWER = 122
     enumerator :: HIPSOLVER_FILL_MODE_FULL = 123
   end enum
 
+  ! hipblasDiagType_t
+  enum, bind(c)
+    enumerator :: HIPSOLVER_DIAG_NON_UNIT = 131
+    enumerator :: HIPSOLVER_DIAG_UNIT = 132
+  end enum
+
+  ! hipblasSideMode_t
   enum, bind(c)
     enumerator :: HIPSOLVER_SIDE_LEFT = 141
     enumerator :: HIPSOLVER_SIDE_RIGHT = 142
+    enumerator :: HIPSOLVER_SIDE_BOTH = 143
   end enum
 
+  ! hipsolverEigMode_t
   enum, bind(c)
     enumerator :: HIPSOLVER_EIG_MODE_NOVECTOR = 201
     enumerator :: HIPSOLVER_EIG_MODE_VECTOR = 202
   end enum
 
+  ! hipsolverEigType_t
   enum, bind(c)
     enumerator :: HIPSOLVER_EIG_TYPE_1 = 211
     enumerator :: HIPSOLVER_EIG_TYPE_2 = 212
     enumerator :: HIPSOLVER_EIG_TYPE_3 = 213
   end enum
 
+  ! hipsolverEigRange_t
   enum, bind(c)
     enumerator :: HIPSOLVER_EIG_RANGE_ALL = 221
     enumerator :: HIPSOLVER_EIG_RANGE_V = 222
     enumerator :: HIPSOLVER_EIG_RANGE_I = 223
   end enum
 
+  ! hipsolverDeterministicMode_t
   enum, bind(c)
     enumerator :: HIPSOLVER_DETERMINISTIC_RESULTS = 241
     enumerator :: HIPSOLVER_ALLOW_NON_DETERMINISTIC_RESULTS = 242
   end enum
 
+  ! hipsolverAlgMode_t
   enum, bind(c)
     enumerator :: HIPSOLVER_ALG_0 = 231
     enumerator :: HIPSOLVER_ALG_1 = 232
   end enum
 
+  ! hipsolverDnFunction_t
   enum, bind(c)
     enumerator :: HIPSOLVERDN_GETRF = 0
   end enum
- 
+
+  ! hipsolverRfFactorization_t
   enum, bind(c)
     enumerator :: HIPSOLVERRF_FACTORIZATION_ALG0 = 0
     enumerator :: HIPSOLVERRF_FACTORIZATION_ALG1 = 1
     enumerator :: HIPSOLVERRF_FACTORIZATION_ALG2 = 2
   end enum
 
+  ! hipsolverRfMatrixFormat_t
   enum, bind(c)
     enumerator :: HIPSOLVERRF_MATRIX_FORMAT_CSR = 0
     enumerator :: HIPSOLVERRF_MATRIX_FORMAT_CSC = 1
   end enum
 
+  ! hipsolverRfNumericBoostReport_t
   enum, bind(c)
     enumerator :: HIPSOLVERRF_NUMERIC_BOOST_NOT_USED = 0
     enumerator :: HIPSOLVERRF_NUMERIC_BOOST_USED = 1
   end enum
 
+  ! hipsolverRfResetValuesFastMode_t
   enum, bind(c)
     enumerator :: HIPSOLVERRF_RESET_VALUES_FAST_MODE_OFF = 0
     enumerator :: HIPSOLVERRF_RESET_VALUES_FAST_MODE_ON = 1
   end enum
 
+  ! hipsolverRfTriangularSolve_t
   enum, bind(c)
     enumerator :: HIPSOLVERRF_TRIANGULAR_SOLVE_ALG1 = 1
     enumerator :: HIPSOLVERRF_TRIANGULAR_SOLVE_ALG2 = 2
     enumerator :: HIPSOLVERRF_TRIANGULAR_SOLVE_ALG3 = 3
   end enum
 
+  ! hipsolverRfUnitDiagonal_t
   enum, bind(c)
     enumerator :: HIPSOLVERRF_UNIT_DIAGONAL_STORED_L = 0
     enumerator :: HIPSOLVERRF_UNIT_DIAGONAL_STORED_U = 1
@@ -125,8 +150,8 @@ module hipfort_hipsolver_enums
     enumerator :: HIPSOLVERRF_UNIT_DIAGONAL_ASSUMED_U = 3
   end enum
 
-#ifdef USE_FPOINTER_INTERFACES
+  integer(c_int), parameter :: hipsolverVersionMajor = 3
+  integer(c_int), parameter :: hipsolverVersionMinor = 2
+  integer(c_int), parameter :: hipsolverVersionPatch = 0
 
-  
-#endif
 end module hipfort_hipsolver_enums
